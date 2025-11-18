8c29031ffbc0187ab6ecbe427380cb7125d5d5bf
권유석 <kyus0919@gmail.com>
2025-11-18 17:53:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.50 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,11 @@
+def solution(left, right):
+    answer = 0
+    
+    for i in range(left, right + 1):
+        sqrt = i ** 0.5
+        
+        if int(sqrt) == sqrt:
+            answer -= i
+        else:
+            answer += i
+    return answer
