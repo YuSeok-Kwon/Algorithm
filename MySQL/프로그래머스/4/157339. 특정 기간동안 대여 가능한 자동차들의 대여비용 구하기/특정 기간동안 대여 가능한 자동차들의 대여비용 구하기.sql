d44cd0ecf7b2fc98b8f2f31c3c4715e64cee1592
권유석 <kyus0919@gmail.com>
2025-11-24 10:47:49 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,24 +1,38 @@
+-- 코드를 입력하세요
 WITH unavailable AS (
-    SELECT car_id
-    FROM car_rental_company_rental_history
-    WHERE start_date <= '2022-11-30' AND end_date >= '2022-11-01'
+    SELECT
+        car_id
+    FROM
+        car_rental_company_rental_history
+    WHERE
+        start_date <= '2022-11-30'
+        AND end_date >= '2022-11-01'
+),
+available AS (
+    SELECT
+        c.car_id
+        , c.car_type
+        , c.daily_fee
+    FROM
+        car_rental_company_car c
+    LEFT JOIN
+        unavailable u ON c.car_id = u.car_id
+    WHERE
+        u.car_id IS NULL
+        AND car_type IN ('세단','SUV')
 )
 
-SELECT 
-    c.car_id,
-    c.car_type,
-    ROUND(c.daily_fee * 30 * (100 - p.discount_rate) / 100, 0) AS fee
-FROM 
-    car_rental_company_car c
-JOIN 
-    car_rental_company_discount_plan p ON c.car_type = p.car_type
-WHERE 
-    c.car_type IN ('세단', 'SUV') 
-    AND p.duration_type = '30일 이상'
-    AND c.car_id NOT IN (SELECT car_id FROM unavailable)
-HAVING 
-    fee >= 500000 AND fee < 2000000
-ORDER BY 
-    fee DESC, 
-    c.car_type ASC, 
-    c.car_id DESC;
+SELECT
+    car_id
+    , a.car_type
+    , ROUND(daily_fee * 30 * (100 - discount_rate) * 0.01, 0) AS fee
+FROM
+    available a
+JOIN
+    car_rental_company_discount_plan p ON a.car_type = p.car_type
+WHERE
+    duration_type = '30일 이상'
+    AND (ROUND(daily_fee * 30 * (100 - discount_rate) * 0.01, 0) >= 500000
+    AND ROUND(daily_fee * 30 * (100 - discount_rate) * 0.01, 0) < 2000000)
+ORDER BY
+    fee DESC, car_type, car_id DESC
