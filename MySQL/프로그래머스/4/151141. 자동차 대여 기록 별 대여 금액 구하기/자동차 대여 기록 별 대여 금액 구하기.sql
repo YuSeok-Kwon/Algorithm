59cd9731df21e5500570c841440ff32e79b4e3f8
권유석 <kyus0919@gmail.com>
2025-11-24 11:54:50 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,33 +1,49 @@
--- 코드를 입력하세요
-WITH discount AS (
+WITH discount_rate AS (
     SELECT
-         c.car_type
-        , history_id
-        , daily_fee
-        , DATEDIFF(end_date, start_date) + 1 AS diffdate
-        , CASE
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-            WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-            ELSE '할인 없음'
-        END AS duration
+        car_type
+        , MAX(CASE WHEN duration_type = '7일 이상'
+                   THEN CAST(REPLACE(discount_rate, '%', '') AS DECIMAL(5,2)) / 100
+                   ELSE NULL END) AS r7
+        , MAX(CASE WHEN duration_type = '30일 이상'
+                   THEN CAST(REPLACE(discount_rate, '%', '') AS DECIMAL(5,2)) / 100
+                   ELSE NULL END) AS r30
+        , MAX(CASE WHEN duration_type = '90일 이상'
+                   THEN CAST(REPLACE(discount_rate, '%', '') AS DECIMAL(5,2)) / 100
+                   ELSE NULL END) AS r90
+    FROM
+        car_rental_company_discount_plan
+    GROUP BY
+        car_type
+),
+only_truck AS (
+    SELECT
+        h.history_id  -- [추가] 여기서 뽑아야 나중에 씁니다!
+        , c.car_type
+        , c.daily_fee
+        , DATEDIFF(end_date, start_date) + 1 AS days
     FROM
         car_rental_company_car c
     JOIN
-        car_rental_company_rental_history h ON c.car_id = h.car_id
+        car_rental_company_rental_history h ON c.car_id = h.car_id -- [수정] hitory -> history
     WHERE
         c.car_type = '트럭'
 )
 
 SELECT
-    history_id
-    , ROUND(daily_fee * diffdate * (100 - COALESCE(p.discount_rate, 0)) * 0.01, 0) AS fee
+    t.history_id
+    , CAST(
+        t.daily_fee * t.days * (1 - COALESCE(
+            CASE 
+                WHEN t.days >= 90 THEN d.r90
+                WHEN t.days >= 30 THEN d.r30
+                WHEN t.days >= 7  THEN d.r7
+                ELSE 0 
+            END, 0)
+        ) AS SIGNED
+      ) AS fee
 FROM
-    discount d
-LEFT JOIN
-    car_rental_company_discount_plan p
-ON 
-    p.car_type = d.car_type
-    AND p.duration_type = d.duration
+    only_truck t
+JOIN
+    discount_rate d ON t.car_type = d.car_type
 ORDER BY
     fee DESC, history_id DESC
